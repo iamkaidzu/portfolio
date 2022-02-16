a77fdc0be15fbd073acf1fc8b9fe7caebf3ddc5e
iamkaidzu <kaizu.kousuke@gmail.com>
2022-02-16 23:23:14 +0900

    ver1.0
    New Reposiotry

---
+++ js/base.js
@@ -0,0 +1,14 @@
+'use strict';
+{
+    document.head.insertAdjacentHTML('afterbegin' ,`
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- FontAwesome -->
+    <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!-- Google font Icon -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    `);
+}
